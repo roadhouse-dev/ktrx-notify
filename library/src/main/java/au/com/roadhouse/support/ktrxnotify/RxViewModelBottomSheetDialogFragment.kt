7a3e6e195fb6d2d1f77a519e6be311674ea268fc
Dwayne Hoy <dwayne@roadhouse.com.au>
2018-06-25 16:16:29 +1000

    Added ViewModelOwner base class for BottomSheets

---
+++ library/src/main/java/au/com/roadhouse/support/ktrxnotify/RxViewModelBottomSheetDialogFragment.kt
@@ -0,0 +1,34 @@
+package au.com.roadhouse.support.ktrxnotify
+
+import android.os.Bundle
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+
+abstract class RxViewModelBottomSheetDialogFragment<V: RxViewModel>: RxBottomSheetDialogFragment(), RxViewModelOwner<V> {
+
+    private lateinit var notificationDisposable: CompositeDisposable
+    private lateinit var viewModel: V
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = onCreateViewModel()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        notificationDisposable = CompositeDisposable()
+        viewModel.notificationObservable
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy {
+                    onViewModelNotification(it)
+                }
+                .addTo(notificationDisposable)
+    }
+
+    override fun onPause() {
+        notificationDisposable.dispose()
+        super.onPause()
+    }
+}
